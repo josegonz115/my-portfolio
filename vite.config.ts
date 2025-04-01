3f31cca84eb013ebc8423eb63354c1086f21db7e
Jose <josegonz115@gmail.com>
2025-04-01 16:54:45 -0700

    updated libs + migrated to bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    tailwindcss(),
   ],
   assetsInclude: ['**/*.JPG'],
 })
